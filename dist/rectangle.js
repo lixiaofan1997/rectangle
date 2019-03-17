08c8ce6f683517ae9e2ad2f3e0162393e4f4a7a0
lixiaofan1997 <773515092@qq.com>
2019-03-17 20:25:41 +0800

    矩形计算器v0.5

--- dist/rectangle.js
+++
@@ -1,1 +0,0 @@
-$(function(){var e=$("#width"),r=$("#height"),a=$("#calculate"),l=$("#perimeter"),t=$("#area");a.click(function(){var a=new rectangle(e.val(),r.val());t.val(a.area()),l.val(a.perimeter())})});
